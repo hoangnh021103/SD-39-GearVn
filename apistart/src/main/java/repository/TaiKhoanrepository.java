6c18888660c279d6bdc46071c18249de384117e9
hoangnh021103 <125101746+hoangnh021103@users.noreply.github.com>
2025-02-13 12:28:45 +0700

    Delete apistart directory

--- apistart/src/main/java/repository/TaiKhoanrepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import Entity.TaiKhoan;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaiKhoanrepository extends JpaRepository<TaiKhoan,Integer> {
-}
