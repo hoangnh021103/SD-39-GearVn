8fa0174cf7c64029e2205181c3d96d18f42e9911
khai520 <45537943+khai520@users.noreply.github.com>
2025-04-02 14:27:11 +0700

    Add files via upload

---
+++ test/java/com/example/apistart/ApistartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apistart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApistartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
